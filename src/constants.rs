8e47cafef41cf8ff96b780ed5064d4d0e6dfd9c0
noxim <aaro.peramaa@gmail.com>
2018-04-03 20:09:03 +0200

    Trying to figure out why core_1 causes lockdown, but sdl is now fully configurable

--- src/constants.rs
+++ src/constants.rs
@@ -21,9 +21,22 @@ pub const IO_MOUSE: i64 = 0x13FFF7A0; // mouse data beginning
 // 8 bytes power of touch, 2^32 is 1kg
 // 8 bytes scroll wheel, -1 is up and 1 is down
 
-pub const CPU_0_FLAGS: i64 = 0x13EE0000;
-pub const CPU_0_PC: i64 = CPU_0_FLAGS + 8;
+pub const CPU_0_FLAGS: i64 = 0x13EE0000 + 0;
+pub const CPU_0_PC: i64    = 0x13EE0000 + 8;
 // 8 bytes cpu state, 0 no cpu, 1 running 2 stop requested 4 stopped
 // 8 bytes program counter
-pub const CPU_1_FLAGS: i64 = CPU_0_PC + 8;
-pub const CPU_1_PC: i64 = CPU_1_FLAGS + 8;
+pub const CPU_1_FLAGS: i64 = 0x13EE0000 + 16;
+pub const CPU_1_PC: i64    = 0x13EE0000 + 24;
+pub const CPU_2_FLAGS: i64 = 0x13EE0000 + 32;
+pub const CPU_2_PC: i64    = 0x13EE0000 + 40;
+pub const CPU_3_FLAGS: i64 = 0x13EE0000 + 48;
+pub const CPU_3_PC: i64    = 0x13EE0000 + 56;
+
+// CPU States
+pub const CPU_NOT_PRESENT: i64    = 0;
+pub const CPU_RUNNING: i64        = 1;
+pub const CPU_STOP_REQUESTED: i64 = 2;
+pub const CPU_STOPPED: i64        = 4;
+pub const CPU_SHUTDOWN: i64       = 8;
+pub const CPU_RESET: i64          = 16;
+pub const CPU_SUSPEND_RAM: i64    = 32;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(core_intrinsics)]
+#![feature(const_fn)]
 
 #[cfg(feature = "sdl")]
 extern crate sdl2;
@@ -55,9 +56,24 @@ fn write_ptr(base: *mut u8, s: i64, address: i64) {
 
 #[inline(always)]
 fn read(mem: &Vec<u8>, address: i64) -> i64 {
+    
     let base = (&mem[..]).as_ptr();
     let addr = unsafe { base.offset(address as isize) as *const i64 };
     unsafe { bswap(*(addr as *const i64) ) }
+    /*
+    use std::mem::transmute;
+    let x: [u8; 8] = [
+        mem[address as usize + 0],
+        mem[address as usize + 1],
+        mem[address as usize + 2],
+        mem[address as usize + 3],
+        mem[address as usize + 4],
+        mem[address as usize + 5],
+        mem[address as usize + 6],
+        mem[address as usize + 7],
+    ];
+    unsafe { bswap(transmute(x)) }
+    */
 }
 
 #[inline(always)]
@@ -87,111 +103,176 @@ fn main() {
     #[cfg(feature = "sdl")] let context = sdl2::init().unwrap();
     #[cfg(feature = "sdl")] let video = context.video().unwrap();
     #[cfg(feature = "sdl")] let window = video.window("Nihongo", width, height)
-        .position_centered()
-        .build().unwrap();
+                                .position_centered()
+                                .build().unwrap();
     #[cfg(feature = "sdl")] let mut canvas = window.into_canvas().build().unwrap();
     #[cfg(feature = "sdl")] let tex_creator = canvas.texture_creator();
     #[cfg(feature = "sdl")] let mut tex = tex_creator.create_texture_streaming(
-        Some(PixelFormatEnum::RGB24), // pixel format
-        width, // dimens
-        height
-    ).unwrap();
+                                Some(PixelFormatEnum::RGB24), // pixel format
+                                width, // dimens    
+                                height).unwrap();
     #[cfg(feature = "sdl")] canvas.clear();
     #[cfg(feature = "sdl")] canvas.present();
     #[cfg(feature = "sdl")] let mut pump = context.event_pump().unwrap();
-    
-
-    // vm variables
-    let mut pc1 = 0i64;
-    let mut pc2 = 0i64;
 
     let mut iter = 0usize;
     let mut ins = 0usize;
     let mut test = 0usize;
     let mut last_frame = 0i64;
 
-    // initialize second core
-    let mut state2 = 4;
+    write(bin, CPU_RUNNING, CPU_0_FLAGS); // Cpu 0 starts off running
+    write(bin, CPU_STOPPED, CPU_1_FLAGS); // State is stopped
+    write(bin, CPU_NOT_PRESENT, CPU_2_FLAGS); // State is stopped
+    write(bin, CPU_NOT_PRESENT, CPU_3_FLAGS); // State is stopped
+    write(bin, 0, CPU_0_PC); // start 0
+    write(bin, 0, CPU_1_PC); // start 1
+    write(bin, 0, CPU_2_PC); // start 2
+    write(bin, 0, CPU_3_PC); // start 3
 
-    write(bin, state2, CPU_1_FLAGS); // state is stopped
-    write(bin, pc2, CPU_1_PC); // start 0
-
-    println!("VM state initialized, launching");
+    println!("VM state initialized");
     
     //PROFILER.lock().unwrap().start("./nihongo.profile").expect("Profile failed");
     let start = Instant::now();
-    
-    /*
-    let mem = (&bin[..]).as_ptr();
-    let mem_mut = (&mut bin[..]).as_mut_ptr();
-    */
 
     // program loop
     'main: loop {
+
+        //let mut ran_0 = false;
+        /*
+        let a_addr = read(bin, pc0 + 0);
+        let b_addr = read(bin, pc0 + 8);
+        let c_addr = read(bin, pc0 + 16);
+        let a = read(bin, a_addr);
+        let b = read(bin, b_addr);
+        let s = b - a;
+        write(bin, s, b_addr);
+        if s <= 0 {
+            pc0 = c_addr;
+        } else {
+            pc0 += 24;
+        }
+        ins += 1;
+        */
+
         
-        { // CPU_0
-            let mut pc = pc1;
+        match read(bin, CPU_0_FLAGS) { // CPU_0
+            //CPU_NOT_PRESENT => { write(bin, CPU_RUNNING, CPU_0_FLAGS); }, // Ideally this shouldn't happen, but Dawn is buggy?
+            CPU_STOP_REQUESTED | // Should we worry about core 0?
+            CPU_STOPPED => { write(bin, CPU_RUNNING, CPU_0_FLAGS); },
+            CPU_SHUTDOWN => { println!("Graceful shutdown"); break 'main; },
+            CPU_RESET => { println!("Performing hard reset, 0x10 written to CPU_0 flags"); unimplemented!() },
+            //CPU_RUNNING | 
+            flags => {
+                let mut pc = read(bin, CPU_0_PC);
 
-            let pc_restore = pc;
-            let a_addr = read(bin, pc +  0);
-            let b_addr = read(bin, pc +  8);
-            pc         = read(bin, pc + 16);
+                let pc_restore = pc;
+                let a_addr = read(bin, pc +  0);
+                let b_addr = read(bin, pc +  8);
+                pc         = read(bin, pc + 16);
 
-            let a = read(bin, a_addr);
-            let b = read(bin, b_addr);
+                let a = read(bin, a_addr);
+                let b = read(bin, b_addr);
 
-            let s = b - a;
-            write(bin, s, b_addr);        
+                let s = b - a;
+                write(bin, s, b_addr);
+                if b_addr == CPU_1_PC {
+                    println!("CPU_0 wrote to CPU_1 flags??");
+                }
 
-            // wait.. we didn't jump! go back to where we came from and go 24
-            if unsafe { unlikely(s > 0) } {
-                pc = pc_restore + 24;
+                // wait.. we didn't jump! go back to where we came from and go 24
+                if unsafe { unlikely(s > 0) } {
+                    pc = pc_restore + 24;
+                }
+                write(bin, pc, CPU_0_PC);
+
+                ins += 1;
             }
-            write(bin, pc, CPU_0_PC);
-            pc1 = pc;
-
-            ins += 1;
         }
+        
+        match read(bin, CPU_1_FLAGS) { // CPU_1
+            CPU_NOT_PRESENT | // This is disabled, don't run
+            CPU_STOPPED => {}, // CPU is asleep, NOP
+            CPU_STOP_REQUESTED => { write(bin, CPU_STOPPED, CPU_1_FLAGS); },
+            CPU_SHUTDOWN => { println!("CPU_1 is not allowed to shutdown the system; Ignored"); },
+            CPU_RESET => { println!("CPU_1 is not allowed to reset the system; Ignored"); },
+            //CPU_RUNNING | 
+            flags => {
+                let mut pc = read(bin, CPU_1_PC);
 
-        let cpu_1_state = read(bin, CPU_1_FLAGS);
-        if cpu_1_state == 1 { // CPU_1
-            let mut pc = pc2;
-            let lpc = read(bin, CPU_1_PC);
-            if pc != lpc {
-                println!("CPU_1 PC was off sync! PC: {:#X}, Mem: {:#X}", pc, lpc);
-                pc = lpc;
+                let pc_restore = pc;
+                let a_addr = read(bin, pc +  0);
+                let b_addr = read(bin, pc +  8);
+                pc         = read(bin, pc + 16);
+
+                let a = read(bin, a_addr);
+                let b = read(bin, b_addr);
+
+                let s = b - a;
+                write(bin, s, b_addr);
+
+                // wait.. we didn't jump! go back to where we came from and go 24
+                if unsafe { unlikely(s > 0) } {
+                    pc = pc_restore + 24;
+                }
+
+                ins += 1;
             }
-
-            let pc_restore = pc2;
-            let a_addr = read(bin, pc +  0);
-            let b_addr = read(bin, pc +  8);
-            pc         = read(bin, pc + 16);
-
-            let a = read(bin, a_addr);
-            let b = read(bin, b_addr);
-
-            let s = b - a;
-            write(bin, s, b_addr);        
-
-            // wait.. we didn't jump! go back to where we came from and go 24
-            if unsafe { unlikely(s > 0) } {
-                pc = pc_restore + 24;
-            }
-
-            write(bin, pc, CPU_1_PC);
-            pc2 = pc;
-
-            ins += 1;
-        } else if unsafe { unlikely(cpu_1_state == 2) } {
-            println!("CPU_1 disabled");
-            write(bin, 4, CPU_1_FLAGS);
         }
+        
+        // write(bin, pc0, CPU_0_PC);
+        // write(bin, pc1, CPU_1_PC);
+        
+
+        /*
+
+        match read(bin, CPU_1_FLAGS) { // CPU_1
+            CPU_RUNNING => {
+                let mut pc = pc1;
+                let lpc = read(bin, CPU_1_PC);
+
+                if !ran_0 {
+                    println!("CPU_1 ran without CPU_0?");
+                }
+
+                if pc != lpc {
+                    println!("CPU_1 PC was off sync! PC: {:#X}, Mem: {:#X}", pc, lpc);
+                    pc = lpc;
+                }
+
+                let pc_restore = pc2;
+                let a_addr = read(bin, pc +  0);
+                let b_addr = read(bin, pc +  8);
+                pc         = read(bin, pc + 16);
+
+                let a = read(bin, a_addr);
+                let b = read(bin, b_addr);
+
+                let s = b - a;
+                write(bin, s, b_addr);        
+
+                // wait.. we didn't jump! go back to where we came from and go 24
+                if unsafe { unlikely(s > 0) } {
+                    pc = pc_restore + 24;
+                }
+
+                write(bin, pc, CPU_1_PC);
+                pc1 = pc;
+
+                ins += 1;
+            },
+            CPU_NOT_PRESENT |
+            CPU_STOPPED
+            => {},
+            CPU_STOP_REQUESTED => write(bin, CPU_STOPPED, CPU_1_FLAGS),
+            v => { println!("CPU_1 state: {}", v); } // Auxiliary cores don't care about shutdown or reset
+        }
+        */
 
         // end of actual emulator
         
         iter += 1;
 
-        if unsafe { unlikely(iter == 2_000_000_000) } {
+        if unsafe { unlikely(iter == 8_000_000_000) } {
             break 'main;
         }
 
@@ -210,7 +291,7 @@ fn main() {
 
         #[cfg(feature = "sdl")]
         {
-        if unsafe { unlikely(iter % 4_000_000_000 == 0) } {
+        if unsafe { unlikely(iter % 4_000_000 == 0) } {
             let time = {
                 let e = start.elapsed();
                 e.as_secs() as f64 + e.subsec_nanos() as f64 / 1_000_000_000.0
@@ -229,7 +310,7 @@ fn main() {
                 .set_title(&format!("Nihongo {}x{}x{} @ {:.2} MIPS, Ins/Clc: {:.3}", 
                 w, h, d, 
                 ins as f64 / time / 1_000_000.0,
-                ins as f64 / iter as f64
+                ins as f64 / iter as f64,
             ));
 
             for event in pump.poll_iter() {
@@ -259,8 +340,10 @@ fn main() {
 
     //PROFILER.lock().unwrap().stop().expect("Can't stop profiler");
 
-    println!("Halted CPU_0, PC: {:#X}", pc1);
-    println!("Halted CPU_1, PC: {:#X}", pc2);
+    println!("Halted CPU_0, PC: {:#X}", read(bin, CPU_0_PC));
+    println!("Halted CPU_1, PC: {:#X}", read(bin, CPU_1_PC));
+    println!("Halted CPU_2, PC: {:#X}", read(bin, CPU_2_PC));
+    println!("Halted CPU_3, PC: {:#X}", read(bin, CPU_3_PC));
     println!("Runtime: {:.2}s, instructions: {} million, cycles {} million", finish, ins / 1_000_000, iter / 1_000_000);
     println!("Average clcs: {:.2}", iter as f64 / finish / 1_000_000.0);
     println!("Average MIPS: {:.2}", ins as f64 / finish / 1_000_000.0);
